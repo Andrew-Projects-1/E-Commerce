d03438045b435e982b61f22040db3bcc3fad8047
Andrew <Andrew.moskowitz19@gmail.com>
2025-02-19 19:12:25 -0600

    Created and mounted newsletter section

---
+++ frontend/src/components/NewsletterBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsletterBox = () => {
+  return (
+    <div className='text-center'>
+      <p className='text-2xl font-medium text-gray-800'>Subscribe Now And Get 20% Off</p>
+    </div>
+  )
+}
+
+export default NewsletterBox
