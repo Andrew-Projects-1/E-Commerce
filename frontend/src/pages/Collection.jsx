066f9240af5dc4ac89c7a22f746edb908ef0b2cc
Andrew <Andrew.moskowitz19@gmail.com>
2025-02-22 18:03:25 -0600

    Added product sort to collections

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets'
+import Title from '../components/Title'
 
 const Collection = () => {
 
@@ -46,6 +47,20 @@ const Collection = () => {
           </div>
         </div>
       </div>
+
+      {/* Right Side */}
+      <div className='flex-1'>
+        <div className='flex justify-between text-base sm:text-2xl mb-4'>
+          <Title text1={'ALL'} text2={'COLLECTIONS'} />
+          {/* Product Sort */}
+          <select className='border-2 border-gray-300 text-sm px-2'>
+            <option value="Relavent">Sort by: Relavent</option>
+            <option value="Low-High">Sort by: Low-High</option>
+            <option value="High-Low">Sort by: High-Low</option>
+          </select>
+        </div>
+      </div>
+
     </div>
   )
 }
