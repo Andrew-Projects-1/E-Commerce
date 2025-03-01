7b5db47e40b326dc0298baf1a223538e0232699a
Andrew <Andrew.moskowitz19@gmail.com>
2025-03-01 11:15:32 -0600

    Added left side images for products

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -15,6 +15,7 @@ const Product = () => {
       if (item._id === productId) {
         setProductData(item);
         setImage(item.image[0]);
+        console.log(item);
         return null;
       }
     })
@@ -33,7 +34,11 @@ const Product = () => {
         {/* Product Images */}
         <div className='flex-1 flex flex-col-reverse gap-2 sm:flex-row'>
           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
-            
+            {
+              productData.image.map((item, index) => (
+                <img src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+              ))
+            }
           </div>
         </div>
 
