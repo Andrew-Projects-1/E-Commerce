3429cbd4c3100d3c7f1a58ef6c72599284e2062c
Andrew <Andrew.moskowitz19@gmail.com>
2025-03-01 10:16:00 -0600

    Added css properties for divs in product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -26,8 +26,19 @@ const Product = () => {
   },[productId])
 
   return productData ? (
-    <div>
-      
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/* Product Data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+
+        {/* Product Images */}
+        <div className='flex-1 flex flex-col-reverse gap-2 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            
+          </div>
+        </div>
+
+      </div>
+    
     </div>
   ) : <div className='opacity-0'></div>
 }
