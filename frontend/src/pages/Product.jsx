29db54ccd1e1de0987342fb848bfa11fffd195df
Andrew <Andrew.moskowitz19@gmail.com>
2025-03-02 17:47:10 -0600

    Added product price to products

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -6,7 +6,7 @@ import { assets } from '../assets/assets';
 const Product = () => {
 
   const {productId} = useParams();
-  const {products} = useContext(ShopContext);
+  const {products, currency} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
 
@@ -56,6 +56,7 @@ const Product = () => {
             <img src={assets.star_dull_icon} alt="" className="w-3 5" />
             <p className='pl-2'>(122)</p>
           </div>
+          <p>{currency}{productData.price}</p>
         </div>
       </div>
     
