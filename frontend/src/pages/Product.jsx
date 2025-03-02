7fcb4d050643fae181bbbcbbd1a05c6e3200f69c
Andrew <Andrew.moskowitz19@gmail.com>
2025-03-02 17:49:07 -0600

    Added products description

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -57,6 +57,7 @@ const Product = () => {
             <p className='pl-2'>(122)</p>
           </div>
           <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+          <p>{productData.description}</p>
         </div>
       </div>
     
