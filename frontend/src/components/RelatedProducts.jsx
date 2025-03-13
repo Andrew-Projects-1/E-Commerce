3b506a21e8e2fa7f04fe5c9e2e6b089a26f6ad53
Andrew <Andrew.moskowitz19@gmail.com>
2025-03-12 20:40:08 -0700

    Added state variables and minor logic to related products

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const RelatedProducts = ({category, subCategory}) => {
 
+  const {products} = useContext(ShopContext);
+  const [related, setRelated] = useState([]);
   
+  useEffect(() => {
+    
+    if (products.length > 0) {
+      
+      let productsCopy = products.slice();
+    }
+
+  },[products])
 
   return (
     <div>
